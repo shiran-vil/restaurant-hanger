ea415f1abfb8cf87d3f6f5c10a33c2e79d188494
your Name <shiran9955@gmail.com>
2022-04-02 18:54:13 +0300

    fix search

--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -1,21 +1,22 @@
 import React, { useContext, useEffect } from "react";
-import { Link, useSearchParams  } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { RestaurantsContext } from "../context/RestaurantsContext";
 import RestaurantFinder from "../apis/RestaurantFinder";
 import StarRating from "./StarRating";
 
 
 const SearchBox = (props) => {
-   const [searchParams, setSearchParams] = useSearchParams();
-    const search_query = searchParams.get('search_query')
+  const [searchParams, setSearchParams] = useSearchParams();
+  const search_query = searchParams.get('search_query')
+  const params = new URLSearchParams([['search_query',  `${search_query}`]]);
   const { restaurants, setRestaurants } = useContext(RestaurantsContext);
   console.log(search_query);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await RestaurantFinder.get(`?search_query=${search_query}`);
-        
+        const response = await RestaurantFinder.get("/", { params });
+
         console.log(response.data.data);
         setRestaurants(response.data.data.restaurants);
       } catch (err) {
@@ -39,10 +40,10 @@ const SearchBox = (props) => {
   };
 
   return (
-      
+
     <div className="list-group content-wrap">
       <h2 className="text-center">Search Results for '{search_query}'</h2>
-   
+
       <table className="table table-hover table-dark">
         <thead>
           <tr className="bg-primary">
@@ -50,23 +51,23 @@ const SearchBox = (props) => {
             <th scope="col">Location</th>
             <th scope="col">Price Range</th>
             <th scope="col">Ratings</th>
-            
+
           </tr>
         </thead>
         <tbody>
           {restaurants &&
             restaurants.map((restaurant) => {
               return (
-                <tr key={restaurant.name}> 
+                <tr key={restaurant.name}>
                   <td>{restaurant.name}</td>
                   <td>{restaurant.location}</td>
                   <td>{"$".repeat(restaurant.price_range)}</td>
                   <td>{renderRating(restaurant)}</td>
-                  
+
                 </tr>
               );
             })}
-         
+
         </tbody>
       </table>
       <Link to={"/"}>Back to all restaurants</Link>
